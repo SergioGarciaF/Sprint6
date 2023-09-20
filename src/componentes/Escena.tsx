7e144fd0c5fca4717d41aa4c4907248a36ab6799
SergioGarciaF <sergiogarciafernandez4@gmail.com>
2023-09-20 12:09:31 +0200

    Nivell-1-Exercici-3

--- src/componentes/Escena.tsx
+++ src/componentes/Escena.tsx
@@ -1,9 +1,23 @@
-  interface EscenaProps {
-    texto: string;
-  }
-  
-  const Escena: React.FC<EscenaProps> = ({ texto }) => {
-    return <p>{texto}</p>;
-  };
-  
-  export default Escena;
+import styled from 'styled-components';
+
+const EscenaStyled = styled.p`
+  text-align: center;
+  padding: 0.8rem;
+  border: 2px solid black;
+  border-radius: 1rem;
+  box-shadow: 0rem 0rem 0.2rem 0.1rem rgba(0, 0, 0, 0.2);
+`;
+
+interface EscenaProps {
+  texto: string;
+}
+
+const Escena: React.FC<EscenaProps> = ({ texto }) => {
+  return (
+    <EscenaStyled>
+      {texto}
+    </EscenaStyled>
+  );
+};
+
+export default Escena;
